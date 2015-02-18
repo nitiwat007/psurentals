f99ac459e99dfbbae7ffec22c61c341f3f7a76d4
nitiwat007 <nitiwat007@gmail.com>
2015-02-18 09:35:41 +0700

    Delete Temp File

--- psurentals/js/myjs/detail.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    Galleria.loadTheme('js/galleria/themes/classic/galleria.classic.min.js');
-    Galleria.run('.galleria', {responsive:true,height:0.5625});
-});
-
-
