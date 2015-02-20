4af2dbd1e9f813e979c2bab30b7fafd25d0785a1
Beboy208 <beboy208@gmail.com>
2015-02-20 10:02:01 +0700

    ReUploadMissingFile

--- psurentals_source/app/controllers/RentalsControllerNR.php
+++ psurentals_source/app/controllers/RentalsControllerNR.php
@@ -2,65 +2,67 @@
 
 class RentalsControllerNR extends BaseController {
 
-     public function Index() {
-         
-        $ppttype = Input::get('ppttype');
-        $near = Input::get('near');
-        $fee = Input::get('fee');
-        
-        //return sprintf("%s %s %s", $ppttype, $near, $fee);
-               
-        $searchResults = $this->basicSearchRentals($ppttype, $near, $fee);
-        
-        //return $searchResults;
-        
-        return View::make('result', ['rentals' => $searchResults, 'count' => count($searchResults)]);
-    }
-    
-
     private function getApprovedRentals($filter, $orderby, $orderdirection) {
-		
         $whereStatement = "Status <> 'rap'";
-        if (!($filter == '' || ctype_space($filter)))
-        {
+        if (!($filter == '' || ctype_space($filter))) {
             $whereStatement = $whereStatement . " and " . $filter;
         }
-        
+
         $results = DB::select("select * from vrental 
-                where ? order by ? ?", array($filter,  $orderby, $orderdirection));
-                
+                where ? order by ? ?", array($filter, $orderby, $orderdirection));
+
         return Response::json(array('result' => $results));
     }
 
     public function basicSearchRentals($propTypeID, $nearCampus, $rentalFeeUnder) {
-        if ($nearCampus === '' || ctype_space($nearCampus) ||
-            $rentalFeeUnder === '' || ctype_space($rentalFeeUnder) ||
-            $propTypeID === '' || ctype_space($propTypeID))
-        {
-               App::abort(404, 'Invalid Arguments');
+        $config = new ConfigurationController();
+
+        if ($nearCampus === '' || ctype_space($nearCampus) || !is_numeric($nearCampus)) {
+            $nearCampus = $config->getDefaultCampusID();
         }
-    
+
+        if ($rentalFeeUnder === '' || ctype_space($rentalFeeUnder) ||
+                $propTypeID === '' || ctype_space($propTypeID)) {
+            App::abort(404, 'Invalid Arguments');
+        }
+
         $amphoeObj = DB::table('vcampus')
-                                    ->where('id','=',$nearCampus)
-                                    ->select('AmphoeID')
-                                    ->first();
-        
-        if (is_null($amphoeObj) /*|| count($amphoeObj)<= 0*/) {
+                        ->where('id', '=', $nearCampus)
+                        ->select('AmphoeID')->first();
+
+        if (is_null($amphoeObj) /* || count($amphoeObj)<= 0 */) {
             App::abort(404, 'Campus not found');
         }
-        
+
         $amphoeID = $amphoeObj->AmphoeID;
         //return $amphoeObj->AmphoeID;
         //return var_dump($amphoeObj->AmphoeID);
-       
-        $results = DB::table('vrental')
-                            ->where('PropertyTypeID', '=', $propTypeID)
-                            ->where('AmphoeID','=', $amphoeID)
-                            ->Where('MonthlyRentalFeeFrom', '<=',$rentalFeeUnder)
-                            ->Where('MonthlyRentalFeeTo', '>=',$rentalFeeUnder)
-                            ->get();
-        /* @var $results type */
-        return  $results; 
+
+        $query = DB::table('vrental')
+                ->join('vrentalcover', 'vrental.RentalID', '=', 'vrentalcover.RID')
+                ->where('Status', '=', 'rwt')
+                ->where('PropertyTypeID', '=', $propTypeID)
+                ->where('AmphoeID', '=', $amphoeID);
+        if ($rentalFeeUnder > 0) {
+            $query = $query->Where('MonthlyRentalFeeFrom', '<=', $rentalFeeUnder)
+                    ->Where('MonthlyRentalFeeTo', '>=', $rentalFeeUnder);
+        }
+
+        //return $query->paginate($config->getListPerPage());
+        return $query->select('vrental.*' , 'vrentalcover.Picture')->get();
         //return Response::json(array('result' => $results));
+    }
+    
+    public function getRentalCoverImage($rentalID) {
+        $pictureObj = DB::table('rentalpictures')
+                ->where('RID','=', $rentalID)
+                ->orderby('IsCover', 'desc')
+                ->first();
+        
+          if (is_null($pictureObj)) {
+              return "#";
+          }
+          
+          return $pictureObj->Picture;
     }
 }
