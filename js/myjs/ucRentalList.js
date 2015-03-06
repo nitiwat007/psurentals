691716e953be1a950907d3108930dee63e89a7a1
nitiwat007 <nitiwat007@gmail.com>
2015-03-06 11:10:20 +0700

    Fixed bug ucRentalList

--- js/myjs/ucRentalList.js
+++ js/myjs/ucRentalList.js
@@ -1,15 +1,22 @@
+var userInfo = JSON.parse(localStorage.getItem("userInfo"));
 $(function () {
-    $(".cover").click(function(event){
+
+    if (userInfo.isAuthentication) {
+        $(".status").show();
+    } else
+        $(".status").hide();
+
+    $(".cover").click(function (event) {
         event.preventDefault();
-        var RentalID=$(this).attr("id");
-        localStorage.setItem("ucRentalListID",RentalID);
-        window.location.href="/detail";
+        var RentalID = $(this).attr("id");
+        localStorage.setItem("ucRentalListID", RentalID);
+        window.location.href = "/detail";
     });
-    $(".title").click(function(event){
+    $(".title").click(function (event) {
         event.preventDefault();
-        var RentalID=$(this).attr("id");
-        localStorage.setItem("ucRentalListID",RentalID);
-        window.location.href="/detail";
+        var RentalID = $(this).attr("id");
+        localStorage.setItem("ucRentalListID", RentalID);
+        window.location.href = "/detail";
     });
 });
 
